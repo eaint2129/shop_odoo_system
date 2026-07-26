36dba7214beec9aacc40444601698f063506d4ce
ETK <eaint2129@gmail.com>
2026-07-26 21:15:45 +0630

    sale_extend(1.0.1), shop_management(1.0.1)

--- sale_extend/__manifest__.py
+++ sale_extend/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name":"Sale Extend",
-    "version":"19.0.1.0.0",
+    "version":"19.0.1.0.1",
     "category":"Sales",
     "summary":"Extend sales order",
     "depends":['sale'],
--- shop_management/__manifest__.py
+++ shop_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name":"Shop Management",
-    "version":"19.0.1.0.0",
+    "version":"19.0.1.0.1",
     "category":"Sales",
     "summary":"Simple shop course example for junior odoo developer.",
     "depends":['product'],
